37a278f74f78ddd774e576314d8b03f56571d7ca
Merlijnmacgillavry <M.A.P.MacGillavry@student.tudelft.nl>
2017-04-25 22:09:39 +0200

    2-stars
    Terran SCV's are made(8)

---
+++ TerranCommandCenter/TerranCommandCenter.pl
@@ -0,0 +1,9 @@
+:-dynamic friendly/3,
+	resources/4,
+	train/1,
+	unit/2,
+	scvAmount/1.
+	scvCost(100).
+	amountMax(7).
+
+
---
+++ TerranSCV/TerranSCV.pl
@@ -0,0 +1,5 @@
+:-dynamic self/1,		% our own ID from the self/5 percept
+	mineralField/2, 	% mineralField IDs and amounts from the mineralField/5 percept
+	workerActivity/1,	% our own activity state from the workerActivity/1 percept
+	gathered/1,		% used as a goal to gather mineralFields
+	resources/4. 		%amount of resources there are currently available
