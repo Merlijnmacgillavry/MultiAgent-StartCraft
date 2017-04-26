1f211c0ad79f658db5a4f33b0dcf0c38b99f5f3b
Merlijn351 <M.A.P.MacGillavry@student.tudelft.nl>
2017-04-26 15:12:54 +0200

    groundwork 3stars

--- TerranCommandCenter/TerranCommandCenter.pl
+++ TerranCommandCenter/TerranCommandCenter.pl
@@ -2,8 +2,9 @@
 	resources/4,
 	train/1,
 	unit/2,
-	scvAmount/1.
+	scvAmount/1,
+	amountMax/1.
 	scvCost(100).
-	amountMax(7).
+
 
 
--- TerranSCV/TerranSCV.pl
+++ TerranSCV/TerranSCV.pl
@@ -2,4 +2,6 @@
 	mineralField/2, 	% mineralField IDs and amounts from the mineralField/5 percept
 	workerActivity/1,	% our own activity state from the workerActivity/1 percept
 	gathered/1,		% used as a goal to gather mineralFields
-	resources/4. 		%amount of resources there are currently available
+	resources/4,		%amount of resources there are currently available
+	refinaryBuild/3,	% used as a goal to build a refinary
+	vespineGeyser/4.	% vespineGeyser IDs and amounts from the vespineGeyser
